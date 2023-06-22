d730d29709816279fa6dde81763698580fae4db6
NickKipshidze <kipshidze.nick@gmail.com>
2023-06-23 00:17:57 +0400

    Added time to splash text

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 
 ANIMATE_OUTPUT = True
@@ -7,8 +8,9 @@ def splash_text() -> str:
     user: str = os.environ["USER"]
     shell: str = os.environ["SHELL"].split("/")[-1]
     distro: str = os.popen("uname -a").read().split(" ")[1].title()
+    current_time: str = datetime.today().strftime("%I:%M:%S %p")
 
-    text: str = f"< User: {user} | Shell: {shell} | Distribution: {distro} >"
+    text: str = f"< User: {user} | Shell: {shell} | Distribution: {distro} | {current_time} >"
     padding = " " * (os.get_terminal_size().columns//2 - len(text)//2)
 
     return f"\n{padding}{text}\n"
